380f44bf7f615e73b7fb1add477386643afa7976
mugoyal03 <goyal0022@gmail.com>
2021-09-30 08:00:33 +0530

    external jar code commented out

--- src/com/example/BitSetTutorial.java
+++ src/com/example/BitSetTutorial.java
@@ -1,14 +1,14 @@
 package com.example;
 
-import org.openjdk.jol.info.ClassLayout;
-import org.openjdk.jol.vm.VM;
+//import org.openjdk.jol.info.ClassLayout;
+//import org.openjdk.jol.vm.VM;
 
 public class BitSetTutorial {
 
     public static void main(String[] args) {
         boolean[] bits = new boolean[1025];
-        System.out.println(VM.current().details());
-        System.out.println(ClassLayout.parseInstance(bits).toPrintable());
+        // System.out.println(VM.current().details());
+        //System.out.println(ClassLayout.parseInstance(bits).toPrintable());
 
         Runtime env = Runtime.getRuntime();
     }
